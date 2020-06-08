d204c75f45e700354dc6f955a26baf30d5456f4f
Todd Crone <twcrone@gmail.com>
2020-06-08 12:33:38 -0400

    Checkpoint...

--- cmd/wman/main.go
+++ cmd/wman/main.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/twcrone/wman/pkg/string"
 )
 
-type Salutation struct {
-	name     string
-	greeting string
-}
+//type Salutation struct {
+//	name     string
+//	greeting string
+//}
 
 func main() {
-	var s = Salutation{"todd", "hello"}
+	//	var s = Salutation{"todd", "hello"}
 	message := "hello go world"
 	var greeting = &message
 	a, b, c := 1, 2, 3
 	foo(&a)
-	fmt.Println(s.greeting, s.name, *greeting, a, b, c)
+	var name = "Todd"
+	fmt.Println(name, *greeting, a, b, c)
+	fmt.Printf("hello %s, your name backward is %q", name, string.Reverse(name))
 }
 
 func foo(count *int) {
